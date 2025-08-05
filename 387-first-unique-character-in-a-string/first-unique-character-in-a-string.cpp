54ed88f4dc807674ce51ad58551109bbe3e11467
pranjal_katoch <pranjalkatoch710@gmail.com>
2025-08-06 02:33:01 +0530

    Time: 14 ms (46.01%) | Memory: 15 MB (22.94%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char , int>um;
+        for (int i = 0; i < s.length(); i++) {
+        um[s[i]]++;  // count frequency
+    }
+    for (int i = 0; i < s.length(); i++){
+        if(um[s[i]]==1){
+            return i ;
+        }
+    }
+     return -1;   
+    }
+};
