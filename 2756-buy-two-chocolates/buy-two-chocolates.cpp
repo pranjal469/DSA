79b93b6cf6f9e0b5db5bf6bba4dd297b1944a1d4
pranjal_katoch <pranjalkatoch710@gmail.com>
2025-08-22 23:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (30.32%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        // Find the two smallest prices
-        int firstMin = INT_MAX, secondMin = INT_MAX;
-        
-        for (int price : prices) {
-            if (price < firstMin) {
-                secondMin = firstMin;
-                firstMin = price;
-            } else if (price < secondMin) {
-                secondMin = price;
-            }
-        }
-        
-        int total = firstMin + secondMin;
-        
-        if (total <= money) {
-            return money - total;
-        }
-        
-        return money;
-    }
+        int left =money;
+      sort(prices.begin(), prices.end());
+
+        if(prices[0]+prices[1]<=left){
+            left=left -(prices[0]+prices[1]);
+        } 
+        return left;
+     }
 };
