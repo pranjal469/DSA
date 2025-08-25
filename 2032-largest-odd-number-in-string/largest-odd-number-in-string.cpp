6c38296fd2dce7fee9c755a5e3c737f56559e1c0
pranjal_katoch <pranjalkatoch710@gmail.com>
2025-08-26 02:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.97%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        // Traverse from the end
-        for (int i = num.size() - 1; i >= 0; i--) {
-            if ((num[i] - '0') % 2 == 1) {
-                // Found an odd digit, return substring
-                return num.substr(0, i + 1);
+        int n = num.size();
+        for(int i =n-1;i>=0;i--){
+            if((num[i]-'0')%2!=0){
+              return num.substr(0,i+1);
             }
         }
-        // No odd digit found
         return "";
     }
 };
